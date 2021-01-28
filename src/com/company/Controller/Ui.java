f16a2acfcb4b267b106cc51a658b6510a39c9e2b
fredrik1992 <klosansa1@.com>
2021-01-28 07:45:08 +0100

    finished ui made it so more can be added

---
+++ src/com/company/Controller/Ui.java
@@ -0,0 +1,2 @@
+package com.company.Controller;public class Ui {
+}
---
+++ src/com/company/Controller/UiChoicesEnums.java
@@ -0,0 +1,15 @@
+package com.company.Controller;
+
+public class UiChoices {
+
+    public static enum LibrarianSwitchChoices {
+        SEE_ALL_BOOKS_AVAILIBLE,
+        SEARCH_USER,
+        QUIT
+    }
+    public  static enum UserSwitchChoices {
+        ADD_BOOK,
+        REMOVE_BOOK,
+        QUIT
+    }
+}
